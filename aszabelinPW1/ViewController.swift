f5dab67f1a0d0d42240b41a00af239312d044480
Забелин Александр Сергеевич <ASeZabelin@sberbank.ru>
2024-03-16 19:24:56 +0300

    Initial Commit

---
+++ aszabelinPW1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  aszabelinPW1
+//
+//  Created by Забелин Александр Сергеевич on 16.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
